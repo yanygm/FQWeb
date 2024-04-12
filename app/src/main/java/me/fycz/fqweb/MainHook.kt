9afc7e871b9eb737fd8c360c51c296ca0c3b5dae
Yany <475016817@msn.cn>
2024-04-12 12:15:12 +0800

    Update

--- app/src/main/java/me/fycz/fqweb/MainHook.kt
+++ app/src/main/java/me/fycz/fqweb/MainHook.kt
@@ -299,7 +299,7 @@ class MainHook : IXposedHookLoadPackage, IXposedHookZygoteInit, IXposedHookInitP
         )
         textview_9.text = Html.fromHtml(
             """
-            <a href="https://github.com/fengyuecanzhu/FQWeb">Github</a>&nbsp;&nbsp;&nbsp;<a href="https://github.com/fengyuecanzhu/FQWeb#免责声明">免责声明</a>&nbsp;&nbsp;&nbsp;<a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=2cgswx48xaTgYmQjSLfH0XNom5n4vm1z&authKey=npnemJO7L6NyLdKvjePLU%2Ffav5v75Q8alXVzCK%2FypGJtFqp1DV35fyukWhhmvTQU&noverify=0&group_code=887847462">QQ群(887847462)</a>
+            
         """.trimIndent()
         )
         textview_9.movementMethod = LinkMovementMethod.getInstance()
